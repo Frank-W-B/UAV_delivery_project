7bbadd717b27ccccf7cb65b07060cd2af5f88ae1
Frank-W-B <frank.burkholder@gmail.com>
2018-01-30 11:20:58 -0700

    Progress on images

--- src/skyline12_labeler.py
+++ src/skyline12_labeler.py
@@ -6,7 +6,7 @@
 # http://ttic.uchicago.edu/%7Esmaji/projects/skylineParsing/skyline12.tar.gz
 
 import numpy as np
-import scipy
+import scipy.io
 import skimage.io
 import matplotlib.pyplot as plt
 import time
@@ -14,13 +14,13 @@ import cProfile
 import pstats
 import StringIO
 
-def make_img_paths(root, folder, city_dict, imgs_per_folder):
+def make_img_paths(root, folder, city_dict, imgs_per_folder, pref, ext):
     """ Returns a numpy array containing paths to all the images in the dataset """
     img_paths = [] 
     for city in sorted(city_dict):
         for img_num in range(1,imgs_per_folder+1):
-            img_path = "".join([root, folder, city, '/', city_dict[city], 
-                                '_',str(img_num),'.jpg'])
+            img_path = "".join([root, folder, city, '/', pref, city_dict[city], 
+                                '_',str(img_num),ext])
             img_paths.append(img_path)     
     return np.array(img_paths)
 
@@ -56,13 +56,38 @@ def print_profiler_output(pr):
     print(s.getvalue())
 
 
-def read_useful_images(img_paths, use):
+def read_images(img_paths, use):
     """ Read in the images that are large enough """
-    imgs_to_use = img_paths[use]
-    imgs = np.array([skimage.io.imread(fname) for fname in imgs_to_use])
-    return imgs
+    paths_to_use = img_paths[use]
+    imgs = np.array([skimage.io.imread(path) for path in paths_to_use])
+    return paths_to_use, imgs
 
+def read_labels(label_paths, use):
+    """ Reads in labels.  Labels are in Matlab .mat format """
+    paths_to_use = label_paths[use]
+    lbls_raw = [scipy.io.loadmat(path)['labels'] for path in paths_to_use]
+    lbls = []
+    for lbl_raw in lbls_raw:
+        lbl = np.zeros((lbl_raw.shape[0], lbl_raw.shape[1], 3), dtype=np.uint8)
+        bmask = lbl_raw == 1 
+        gmask = ~bmask
+        lbl[:,:,1] = gmask * 255
+        lbl[:,:,2] = bmask * 255
+        lbls.append(np.copy(lbl))
+    return np.array(lbls)
 
+def plot_image_and_label(imgs, lbls):
+    i = 0    
+    for img, lbl in zip(imgs, lbls):
+        fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(9, 4))
+        image = axes[0].imshow(img)
+        mask = axes[1].imshow(lbl)
+        fname = 'temp/' + str(i) + '_.png'
+        plt.savefig(fname)
+        plt.close()
+        i += 1
+
+    
 
 if __name__ == '__main__':
     # read in the skyline 12 dataset 
@@ -74,10 +99,33 @@ if __name__ == '__main__':
                  'Philadelphia': '7', 'Seattle': '8', 'Shanghai': '9',
                  'Singapore': '10', 'Tokyo': '11', 'Toronto': '12'}
     
-    img_paths = make_img_paths(root, img_folder, city_dict, 10) 
+    img_paths = make_img_paths(root, img_folder, city_dict, 10,'', '.jpg')
+    label_paths = make_img_paths(root, lbl_folder, city_dict, 10, 'label_', '.mat')
     use = flag_images_to_use(img_paths, 800, 1600)
-    imgs = read_useful_images(img_paths, use)
-    #for i, img in enumerate(imgs):
+    img_paths_to_use, imgs = read_images(img_paths, use)
+    lbls = read_labels(label_paths, use)
+    plot_image_and_label(imgs, lbls)
+#img_num = 2 
+    #img_path = img_paths[img_num]
+    #label_path = label_paths[img_num]
+    #img = skimage.io.imread(img_path)
+    #lbl_mat = scipy.io.loadmat(label_path)
+    #lbl_raw = lbl_mat['labels']
+    #lbl = np.zeros((lbl_raw.shape[0], lbl_raw.shape[1], 3), dtype=np.uint8)
+    #bmask = lbl_raw == 1 
+    #gmask = ~bmask
+    #lbl[:,:,1] = gmask * 255
+    #lbl[:,:,2] = bmask * 255
+    #path_sample_lbl = '/home/frank/Pictures/skyline12/data/labels/sample_label.png'
+    #ex_lbl = skimage.io.imread(path_sample_lbl)
+    #fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(9, 4))
+    #image = axes[0].imshow(img)
+    #mask = axes[1].imshow(lbl)
+    #plt.show()
+
+
+
+#for i, img in enumerate(imgs):
     #    print i 
     #    plt.imshow(img)
     #    plt.show()
